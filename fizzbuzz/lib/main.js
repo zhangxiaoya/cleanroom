5ee07aa615f28adb53c1006436c93eb05ab0ef78
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-02-25 17:44:09 +0800

    complete the basic function, output the correct list

--- fizzbuzz/lib/main.js
+++ fizzbuzz/lib/main.js
@@ -1,6 +1,13 @@
-function main(){
-    
-    
+function main(a,b,c){
+    var result = "";
+    for(var i = 1;i<= 100;++i)
+    {
+    	if(i == 1)
+    		result += i.toString();
+    	else
+    		result += (" " + i.toString());
+    }
+    return result;
 }
 
 module.exports = main;
