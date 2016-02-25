6f29f42178d93ef3e5a3a649b1f6f360fdc62ee0
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-02-25 18:08:09 +0800

    complete more function, satisfy condition 3 and condition 4

--- fizzbuzz/lib/main.js
+++ fizzbuzz/lib/main.js
@@ -1,11 +1,26 @@
+function toCorrectString(num,a,b,c)
+{
+	var result = "";
+	if(num % a == 0)
+		result += "Fizz";
+	if(num % b == 0)
+		result += "Buzz";
+	if(num % c == 0)
+		result += "Whizz";
+	if(result.length == 0)
+		result += num.toString();
+	return  result;
+}
+
+
 function main(a,b,c){
     var result = "";
     for(var i = 1;i<= 100;++i)
     {
     	if(i == 1)
-    		result += i.toString();
+    		result += toCorrectString(i,a,b,c);
     	else
-    		result += (" " + i.toString());
+    		result += (" " + toCorrectString(i,a,b,c));
     }
     return result;
 }
