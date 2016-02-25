adcd846e1b7ecac0c461325f4950c5866b1a391a
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-02-25 17:59:10 +0800

    add second test case

--- fizzbuzz/spec/main_spec.js
+++ fizzbuzz/spec/main_spec.js
@@ -20,4 +20,13 @@ describe("测试描述", function(){
         
         expect(expect_string).to.equal(result);
     });
+
+    it("测试用例1", function(){
+
+        var result = main(3,5,7);
+        var expect_string = '1 2 Fizz 4 Buzz Fizz Whizz 8 Fizz Buzz 11 Fizz 13 Whizz FizzBuzz 16 17 Fizz 19 Buzz FizzWhizz 22 23 Fizz Buzz 26 Fizz Whizz 29 FizzBuzz 31 32 Fizz 34 BuzzWhizz Fizz 37 38 Fizz Buzz 41 FizzWhizz 43 44 FizzBuzz 46 47 Fizz Whizz Buzz' + 
+        ' Fizz 52 53 Fizz Buzz Whizz Fizz 58 59 FizzBuzz 61 62 FizzWhizz 64 Buzz Fizz 67 68 Fizz BuzzWhizz 71 Fizz 73 74 FizzBuzz 76 Whizz Fizz 79 Buzz Fizz 82 83 FizzWhizz Buzz 86 Fizz 88 89 FizzBuzz Whizz 92 Fizz 94 Buzz Fizz 97 Whizz Fizz Buzz';
+        
+        expect(expect_string).to.equal(result);
+    });
 });
