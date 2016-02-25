c2ef29455921036afa4060172e85e5239655c6f2
zhangxiaoya <zhangxiaoya1989@gmail.com>
2016-02-25 18:15:20 +0800

    add test case witch satisfy condition 5

--- fizzbuzz/spec/main_spec.js
+++ fizzbuzz/spec/main_spec.js
@@ -29,4 +29,13 @@ describe("测试描述", function(){
         
         expect(expect_string).to.equal(result);
     });
+
+    it("测试用例1", function(){
+
+        var result = main(3,5,7);
+        var expect_string = '1 2 Fizz 4 Buzz Fizz Whizz 8 Fizz Buzz 11 Fizz Fizz Whizz FizzBuzz 16 17 Fizz 19 Buzz FizzWhizz 22 Fizz Fizz Buzz 26 Fizz Whizz 29 Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Fizz Buzz 41 FizzWhizz Fizz 44 FizzBuzz 46 47 Fizz Whizz Buzz' + 
+        ' Fizz 52 Fizz Fizz Buzz Whizz Fizz 58 59 FizzBuzz 61 62 Fizz 64 Buzz Fizz 67 68 Fizz BuzzWhizz 71 Fizz Fizz 74 FizzBuzz 76 Whizz Fizz 79 Buzz Fizz 82 Fizz FizzWhizz Buzz 86 Fizz 88 89 FizzBuzz Whizz 92 Fizz 94 Buzz Fizz 97 Whizz Fizz Buzz';
+        
+        expect(expect_string).to.equal(result);
+    });
 });
